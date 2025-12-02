5112fc4e4f001c9775e0a33b997afd7fdc93eaf0
benoit80000 <66091237+benoit80000@users.noreply.github.com>
2025-12-02 21:53:38 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,13 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
   },
   plugins: [],
 };
+
 export default config;
